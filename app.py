4b76a4bb30df29f54a120e207865dc7e90b49e8f
TegarCode <105096476+TegarCode@users.noreply.github.com>
2024-05-07 19:53:07 +0700

    app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+# Menampilkan teks 
+st.subheader("VISUALISASI DATA KU")
+st.write("Tegar Oktavianto Simbolon (21082010140)")
+
+st.subheader("")
+st.subheader("Scatter Plot")
+# 1
+# reading the database
+data = pd.read_csv("https://raw.githubusercontent.com/dhavinaocxa/davis-2024/main/tips.csv")
+
+# Scatter plot with day against tip
+fig, ax = plt.subplots()
+scatter = ax.scatter(data['day'], data['tip'])
+
+# Setting the X and Y labels
+plt.xlabel('Day')
+plt.ylabel('Tip')
+
+# showing the plot
+st.pyplot(fig)
+
+st.subheader("")
+st.subheader("Line Plot")
+# 2
+# draw lineplot
+fig, ax = plt.subplots() 
+sns.lineplot(x="sex", y="total_bill", data=data, ax=ax)
+
+# showing the plot
+st.pyplot(fig)
+
+st.subheader("")
+st.subheader("Line Chart")
+# 3
+# plotting the scatter chart
+fig = px.line(data, y='tip', color='sex')
+
+# showing the plot
+st.plotly_chart(fig)
+
+# Bar plot
+fig = plt.figure(figsize=(10, 6))
+sns.barplot(x='day', y='tip', data=data, hue='sex')
+plt.xlabel('Day')
+plt.ylabel('Tip')
+plt.title('Bar Plot')
+plt.legend(title='Sex')
+st.pyplot(fig)
